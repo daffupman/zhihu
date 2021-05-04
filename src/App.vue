a1363e7fb0d1e68883874af19845a3151971686d
daff <wzjin85@foxmail.com>
2021-05-04 16:37:14 +0800

    表单验证组件

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <global-header :user="user"/>
     <column-list :list="list"/>
-    <form>
+    <validate-form @form-submit="onFormSubmit">
       <div class="mb-3">
         <label class="form-label">邮箱地址</label>
         <validate-input :rules="emailRules" placeholder="请输入邮箱地址" type="text"/>
@@ -11,8 +11,7 @@
         <label class="form-label">密码</label>
         <validate-input type="password" placeholder="请输入密码" :rules="passwordRules" v-model="passwordVal"/>
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    </validate-form>
   </div>
 </template>
 
@@ -22,6 +21,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, {ColumnProps} from '@/views/ColumnList.vue'
 import GlobalHeader, {UserProps} from '@/views/GlobalHeader.vue'
 import ValidateInput, {RuleProps} from '@/views/ValidateInput.vue'
+import ValidateForm from '@/views/ValidateForm.vue'
 
 const currentUser: UserProps = {
   isLogin: true,
@@ -71,11 +71,12 @@ export default defineComponent({
   components: {
     ColumnList,
     GlobalHeader,
-    ValidateInput
+    ValidateInput,
+    ValidateForm,
   },
   setup() {
-    const emailVal = ref()
-    const passwordVal = ref()
+    const emailVal = ref('')
+    const passwordVal = ref('')
     const emailRef = reactive({
       val: '',
       error: false,
@@ -88,15 +89,21 @@ export default defineComponent({
     const passwordRules: RuleProps = [
       { type: 'required', message: '密码不能为空' },
     ]
+    const onFormSubmit = (result: boolean) => {
+      console.log(result)
+    }
 
     return {
       list: mockData,
       user: currentUser,
+
       emailRef,
       emailRules,
       emailVal,
       passwordRules,
       passwordVal,
+
+      onFormSubmit
     }
   }
 })
---
+++ src/views/ValidateForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <form class="validate-form-container">
+    <slot/>
+    <div class="submit-area" @click.prevent="submitForm">
+      <slot name="submit">
+        <button type="submit" class="btn btn-primary">提交</button>
+      </slot>
+    </div>
+  </form>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ValidateForm',
+  emits: ['form-submit'],
+  setup(props, context) {
+    const submitForm = () => {
+      context.emit('form-submit', true)
+    }
+    return {
+      submitForm
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
